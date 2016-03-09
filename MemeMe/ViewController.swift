edd90d3493552c39e9fbdbc1c6a39fc1644b123d
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2016-03-09 16:13:35 -0600

    add action to pick from camera
    and combine two actions

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,24 +10,40 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        let isCameraAvailable = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        cameraButton.enabled = isCameraAvailable
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func pickFromAlbum(sender: AnyObject) {
+    func pickImage(source: UIImagePickerControllerSourceType) {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        imagePicker.sourceType = source
         self.presentViewController(imagePicker, animated: true, completion: nil)
-    
     }
     
+    @IBAction func pickFromAlbum(sender: AnyObject) {
+        let source = UIImagePickerControllerSourceType.PhotoLibrary
+        pickImage(source)
+    }
+    
+    @IBAction func pickFromCamera(sender: AnyObject) {
+        let source = UIImagePickerControllerSourceType.Camera
+        pickImage(source)
+    }
+
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             self.imageView.image = image
