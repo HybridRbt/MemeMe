9466b9a3d50933d2f0ad87a30efc5e046031f5c1
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2016-03-10 22:44:41 -0600

    move the Meme struct out
    into an individual file

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Jianfeng Yang on 3/10/16.
+//  Copyright © 2016 Jianfeng Yang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topTextString : String
+    var bottomTextString: String
+    var originalImage: UIImage?
+    var memedImage: UIImage?
+}
