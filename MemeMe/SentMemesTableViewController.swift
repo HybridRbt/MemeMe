5a2f221c47c2fbabd85a3b33e45cd4f53279a295
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2016-03-23 16:18:49 -0500

    add table view controller for all memes

---
+++ MemeMe/SentMemesTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SentMemesTableViewController.swift
+//  MemeMe
+//
+//  Created by Jianfeng Yang on 3/23/16.
+//  Copyright © 2016 Jianfeng Yang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SentMemesTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    // Get the Mems from the appDelegate for the table
+    var memes: [Meme] {
+        return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
+    }
+    
+    // Table View Data Source
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("SentMemeCell")!
+        let meme = memes[indexPath.row]
+        
+        // Use the top string as the text label
+        cell.textLabel?.text = meme.topTextString
+        
+        // Set image to be the memed image
+        cell.imageView?.image = meme.memedImage
+        
+        // If the cell has a detail label, we will put the evil scheme in.
+        if let detailTextLabel = cell.detailTextLabel {
+            detailTextLabel.text = meme.topTextString + meme.bottomTextString
+        }
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let customMeme = self.storyboard!.instantiateViewControllerWithIdentifier("CustomMemeViewController") as! CustomMemeViewController
+        customMeme.meme = memes[indexPath.row]
+        self.navigationController!.pushViewController(customMeme, animated: true)
+    }
+}
+
+
