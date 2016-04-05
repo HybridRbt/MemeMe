a8c80e7e9cc7bb6ab125fdfb807f59715dc70750
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2016-04-05 09:50:15 -0500

    use code instead of segue

--- MemeMe/CustomMemeViewController.swift
+++ MemeMe/CustomMemeViewController.swift
@@ -18,4 +18,34 @@ class CustomMemeViewController: UIViewController {
         super.viewWillAppear(animated)
         self.memedImage.image = meme.memedImage
     }
+    
+    
+    @IBAction func editButtonTapped(sender: AnyObject) {
+        //var memeEditor: EditorViewController
+        
+        let memeEditor = storyboard?.instantiateViewControllerWithIdentifier("EditorView") as! EditorViewController
+        
+        let memeEditorNavi = UINavigationController(rootViewController: memeEditor)
+        
+        memeEditor.openAsEditor = true
+        memeEditor.meme = meme
+        self.presentViewController(memeEditorNavi, animated: true, completion: nil)
+        
+        
+      //  memeEditor.topTextField.text = meme.topTextString
+      //  memeEditor.imageView.image = meme.originalImage
+
+
+    }
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        if segue.identifier == "openMemeForEditing" {
+//            let editor = segue.destinationViewController as! UINavigationController
+//            let editorViewController = editor.topViewController as! EditorViewController
+//            editorViewController.topTextField.text = meme.topTextString
+//            editorViewController.bottomTextField.text = meme.bottomTextString
+//            editorViewController.imageView.image = meme.originalImage
+//            editor.navigationBarHidden = false
+//        }
+//    }
 }
